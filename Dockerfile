1c35bc337465c39e2a2bd5404738314a8e102e23
mozzi <maozza@gmail.com>
2018-06-17 16:50:22 +0300

    check repo

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /workspace/
 
 EXPOSE 3112
 WORKDIR /workspace/
+#RUN "yarn"
+
 CMD ["node", "index.js"]
 
 
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // content of index.js
 const http = require('http')
 const port = 3112
+//const sleep = require('system-sleep');
+const fs = require('fs');
 
 const requestHandler = (request, response) => {
   console.log(request.url)
+
+    if (fs.existsSync('/var/tmp/sleep')) {
+        console.log('Found file');
+  //      sleep(15000);
+    }
   response.end('Hello Node.js test Server')
 }
 
