44e997aa662b1b2aed14cee57568b11c6bca72ab
build user <devops@neurosteer.com>
2018-07-29 21:07:16 +0300

    print env version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const port = 3112
 const sleep = require('system-sleep');
 const fs = require('fs');
-
+const version = process.env.version;
 const requestHandler = (request, response) => {
   console.log(request.url)
 
@@ -11,15 +11,16 @@ const requestHandler = (request, response) => {
         console.log('Found file');
         sleep(15000);
     }
-  response.end('Hello Node.js test Server 123')
+
+  response.end('Hello Node.js test Server, version: ' + version );
 }
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened', err);
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log(`server is listening on ${port}`);
 })
